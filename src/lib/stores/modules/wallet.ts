2d33f2b9bb7367d64afb64444917117e99bea4e3
Marlon Monroy <saito.sv.h@gmail.com>
2022-08-15 12:58:54 -0700

    small format

--- src/lib/stores/modules/wallet.ts
+++ src/lib/stores/modules/wallet.ts
@@ -4,11 +4,11 @@ import { writable, type Writable } from 'svelte/store';
 const { useWeb3Provider, chainStore } = createChainStore();
 export const currentAccount: Writable<string> = writable('');
 export const currentChainID: Writable<number> = writable(-1);
-export const curerntSigner:Writable<any> = writable(null);
+export const curerntSigner: Writable<any> = writable(null);
 
 export const connect = () => {
-	useWeb3Provider(window.ethereum);
-    chainStore.subscribe( (store: any)  => {
+    useWeb3Provider(window.ethereum);
+    chainStore.subscribe((store: any) => {
         console.log(store)
         currentAccount.set(store.account);
         currentChainID.set(store.chainId);
