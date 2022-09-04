443cbc8a5d88f09cc8477fd834f05b5ca4d0a170
Marlon Monroy <saito.sv.h@gmail.com>
2022-09-03 17:22:15 -0700

    text to match text

--- src/lib/components/Address.spec.ts
+++ src/lib/components/Address.spec.ts
@@ -1,20 +1,8 @@
 import {render, fireEvent, screen} from '@testing-library/svelte'
 import Address from './Address.svelte'
 
-it('shows proper heading when rendered', () => {
+it('shows proper text when rendered', () => {
   render(Address)
-  const heading = screen.getByText('Send to')
-  expect(heading).toBeInTheDocument()
+  const txt = screen.getByText('Send to')
+  expect(txt).toBeInTheDocument()
 })
-
-// Note: This is as an async test as we are using `fireEvent`
-// it('changes button text on click', async () => {
-//   render(Comp, {name: 'World'})
-//   const button = screen.getByRole('button')
-
-//   // Using await when firing events is unique to the svelte testing library because
-//   // we have to wait for the next `tick` so that Svelte flushes all pending state changes.
-//   await fireEvent.click(button)
-
-//   expect(button).toHaveTextContent('Button Clicked')
-// })
